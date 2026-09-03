800bd2bb25d40c73d8dc569d63274712df0fd68e
mpondisimb-eng <mpondisimb@gmail.com>
2026-09-03 16:50:33 -0400

    chore(sitemap): add /blog + article slugs

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,16 @@
 import { MetadataRoute } from 'next';
+import { articles } from '@/lib/blog';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://carpoolwork.ca';
   const locales = ['fr', 'en'];
-  const pages = ['', '/faq', '/employers', '/employer/inscription'];
+  const pages = ['', '/faq', '/employers', '/employer/inscription', '/blog'];
+  const blogPages = articles.map((a) => `/blog/${a.slug}`);
 
   const entries: MetadataRoute.Sitemap = [];
 
   for (const locale of locales) {
-    for (const page of pages) {
+    for (const page of [...pages, ...blogPages]) {
       entries.push({
         url: `${baseUrl}/${locale}${page}`,
         lastModified: new Date(),
